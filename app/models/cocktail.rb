88f54853a5887f8933d497c24fe4f2a686516b81
vanessa massiot <vanessa.massiot@free.fr>
2020-05-14 12:55:27 +0200

    model validations

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,6 @@
 class Cocktail < ApplicationRecord
-	has_many :ingredients, :doses
+   has_many :doses, dependent: :destroy
+   has_many :ingredients, :through => :doses
+   validates :name, uniqueness: true, presence: true
+
 end
--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,10 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+
+
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail, scope: :ingredient
 end
