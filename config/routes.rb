e002fc7a560d1320fa60f4d723c7052cf5fb35f0
vanessa massiot <vanessa.massiot@free.fr>
2020-05-17 21:24:08 +0200

    edit and delete on index

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
  resources :cocktails do
  	resources :doses, only: [ :index, :new, :create ]
  end
-
  resources :doses, only: [ :destroy]
+
+ resources :doses do
+ 	resources :ingredients, only: [ :index, :new, :create ]
+ end
+
+ resources :ingredients, only: [ :destroy ]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
